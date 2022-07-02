ffdc2144f33ad4005fc7e12212c9870bc037ad25
unknown <faustosmiriglia@gmail.com>
2022-07-01 22:30:51 -0300

    Datos - Ejercicio 3

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,13 @@
-/*
-Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
-
+	/*
+	Proposito:
+		-Debemos lograr tomar un dato por 'ID'
+		y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'
+	
+	Precondicion:
+		-Ninguna
+	*/
+	let nombreIngresado = txtIdNombre.value;//Toma el dato de la textbox y lo almacena en una variable
+	alert(nombreIngresado);
 }
-
-
