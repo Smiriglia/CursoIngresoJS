2a0398e621f7ddd6875761d86132bfe5da126823
unknown <faustosmiriglia@gmail.com>
2022-07-01 23:10:53 -0300

    Datos - Ejercicio 4

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,14 @@
-/* 
-Debemos lograr tomar un dato por 'PROMPT' 
-y lo muestro por 'ID' al presionar el botón 'mostrar'
-*/
 function mostrar()
 {
-	alert("ok");
-}
+	/* 
+	Proposito:
+		-Debemos lograr tomar un dato por 'PROMPT' 
+		y lo muestro por 'ID' al presionar el botón 'mostrar'
 
+	Precondicion:
+		-Ninguna
+	*/
+
+	//Toma un valor con el prompt y se lo asigna al valor del textbox.
+	txtIdNombre.value = prompt("Ingrese su nombre:")
+}
