e7fd211ad1bdab5d8fe53b6a9b363bec8387bb89
unknown <faustosmiriglia@gmail.com>
2022-07-02 19:50:16 -0300

    Datos - Ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -1,25 +1,90 @@
-/*
-Debemos lograr tomar Los numeros por ID ,
-transformarlos a enteros (parseInt),realizar la operación correcta y 
-mostrar el resulto por medio de "ALERT"
-ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	/*
+	Proposito:
+		-Debemos lograr tomar Los numeros por ID ,
+		transformarlos a enteros (parseInt) y Sumarlos.
+		mostrar el resulto por medio de "ALERT".
+	
+	Precondicion:
+		-Ninguna.
+	*/
+
+	//Toma el valor de la textbox, lo convierte el valor de string a numero y lo asigna en una variable .
+	let num1 = parseInt(txtIdNumeroUno.value);
+	let num2 = parseInt(txtIdNumeroDos.value);
+
+	//Sumamos los 2 valores obtenidos anteriormente.
+	resultado = num1 + num2;
+
+	//Imprime el resultado en pantalla
+	alert(`La suma da como resultado: ${resultado}`);
 }
 
 function restar()
 {
+	/*
+	Proposito:
+		-Debemos lograr tomar Los numeros por ID ,
+		transformarlos a enteros (parseInt) y Restarlos.
+		mostrar el resulto por medio de "ALERT".
+	
+	Precondicion:
+		-Ninguna.
+	*/
+	//Toma el valor de la textbox, lo convierte el valor de string a numero y lo asigna en una variable .
+	let num1 = parseInt(txtIdNumeroUno.value);
+	let num2 = parseInt(txtIdNumeroDos.value);
+
+	//Restamos los 2 valores obtenidos anteriormente.
+	resultado = num1 - num2;
+
+	//Imprime el resultado en pantalla
+	alert(`La resta da como resultado: ${resultado}`);
 	
 }
 
 function multiplicar()
-{ 
+{
+	/*
+	Proposito:
+		-Debemos lograr tomar Los numeros por ID ,
+		transformarlos a enteros (parseInt) y Multiplicarlos.
+		mostrar el resulto por medio de "ALERT".
 	
+	Precondicion:
+		-Ninguna.
+	*/
+	//Toma el valor de la textbox, lo convierte el valor de string a numero y lo asigna en una variable .
+	let num1 = parseInt(txtIdNumeroUno.value);
+	let num2 = parseInt(txtIdNumeroDos.value);
+
+	//multiplicamos los 2 valores obtenidos anteriormente.
+	resultado = num1 * num2;
+
+	//Imprime el resultado en pantalla
+	alert(`El producto es: ${resultado}`);
 }
 
 function dividir()
 {
+	/*
+	Proposito:
+		-Debemos lograr tomar Los numeros por ID ,
+		transformarlos a enteros (parseInt) y Dividirlos.
+		mostrar el resulto por medio de "ALERT".
 	
+	Precondicion:
+		-Ninguna.
+	*/
+	//Toma el valor de la textbox, lo convierte el valor de string a numero y lo asigna en una variable .
+	let num1 = parseInt(txtIdNumeroUno.value);
+	let num2 = parseInt(txtIdNumeroDos.value);
+
+	//Dividimos los 2 valores obtenidos anteriormente.
+	resultado = num1 / num2;
+
+	//Imprime el resultado en pantalla
+	alert(`La division da como resultado: ${resultado}`);
 }
 
