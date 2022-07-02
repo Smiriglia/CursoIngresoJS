fabf6ddce2fdd9ac9fd89a81c9f6d70ad2b437ce
unknown <faustosmiriglia@gmail.com>
2022-07-01 23:32:16 -0300

    Datos - Ejercicio 5

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,8 +1,18 @@
-/*
-Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
-ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
-{	
-	alert("ok");
+{
+	/*
+	Proposito:
+		-Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+		ej.: "Usted se llama José y tiene 66 años"
+		
+	Precondicion:
+		-Ninguna
+	*/
+	//Inicializa las variables con los datos que esten ingresados en sus respectivas textbox
+	let nombre = txtIdNombre.value;
+	let edad = txtIdEdad.value;
+	
+	//Imprime los datos en pantalla concatenando strings
+	alert("Usted se llama " + nombre + " y tiene " + edad + " años");
 }
 
