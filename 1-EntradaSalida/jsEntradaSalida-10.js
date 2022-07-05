80c3e1ef442d4ff8935d551ca2c638c7eb4bc0dc
unknown <faustosmiriglia@gmail.com>
2022-07-05 13:02:41 -0300

    Datos - Ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,9 +1,26 @@
-/*
-Debemos lograr tomar el importe por ID.
-Transformarlo a entero (parseInt), luego
-mostrar el importe con un Descuento del 25 %
-en el cuadro de texto "RESULTADO"*/
+
 function mostrarAumento()
 {
-	alert("ok");
+	/*
+	Proposito:
+		-Debemos lograr tomar el importe por ID.
+		Transformarlo a entero (parseInt), luego
+		mostrar el importe con un Descuento del 25 %
+		en el cuadro de texto "RESULTADO"
+
+	Precondicion:
+		-El valor ingresado en la textbox debe ser de tipo numero.
+	*/
+
+	//Inicializa la variable tomando el valor de la textbox y convirtiendolo en un float.
+	let sueldo = parseFloat(document.getElementById("txtIdImporte").value);
+
+	//Inicializa la variable calculando el 25% de la variable sueldo.
+	let descuento = sueldo * 0.25;
+
+	//Inicializa la variable restando el sueldo con el descuento.
+	let sueldoActualizado = sueldo - descuento;
+
+	//Cambia el valor de la textbox *txtIdResultado* por el sueldo actualizado
+	document.getElementById("txtIdResultado").value = sueldoActualizado;
 }
