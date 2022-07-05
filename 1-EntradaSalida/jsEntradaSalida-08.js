503df77a86bd27cce6a27ea4347258d974606c81
unknown <faustosmiriglia@gmail.com>
2022-07-05 11:33:19 -0300

    Datos - Ejercicio 8

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,9 +1,27 @@
-/*
-Debemos lograr tomar Los numeros por ID ,
-transformarlos a enteros (parseInt),realizar la operación correcta y 
-mostrar el resto entre el dividendo y el divisor.
-ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	/*
+	Poposito:
+	 
+		-Debemos lograr tomar Los numeros por ID ,
+		transformarlos a enteros (parseInt),realizar la operación correcta y 
+		mostrar el resto entre el dividendo y el divisor.
+		ej.: "El resto es 0 ."
+	Precondiciones:
+		-El valor ingresado en txtIdNumeroUno y txtIdNumeroDos deben
+		ser numeros enteros.
+
+		-El valor ingresado en txtIdNumeroDos Debe ser distinto de cero.
+	*/
+
+	//Toma el valor de la textbox, lo convierte el valor de string a numero y lo asigna en una variable .
+	let dividendo = parseInt(txtIdNumeroDividendo.value);
+	let divisor = parseInt(txtIdNumeroDivisor.value);
+	let resultado;
+
+	//Sacamos el resto 2 valores obtenidos anteriormente.
+	resultado = dividendo % divisor;
+
+	//Imprime el resultado en pantalla
+	alert(`La division da como resultado: ${resultado}`);
 }
