8ac5ebeea98d150ed42166bc9ee3fbb9d1e63676
unknown <faustosmiriglia@gmail.com>
2022-07-07 17:47:25 -0300

    Datos - Ejercicio 9

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,3 +1,7 @@
+/*
+	Smiriglia, Fausto DIV Z
+	E/S Ejercicio 9
+*/
 function mostrarAumento()
 {
 	/*
@@ -10,14 +14,18 @@ function mostrarAumento()
 		-El valor ingresado en la textbox debe ser de tipo numero.
 	*/
 
-	//Inicializa la variable tomando el valor de la textbox y convirtiendolo en un float.
-	let sueldo = parseFloat(document.getElementById("txtIdSueldo").value);
+	//declara las variables
+	let sueldo;
+	let aumento;
+	let sueldoActualizado;
+	//asigna a la variable el valor de la textbox y convirtiendolo en un float.
+	sueldo = parseFloat(document.getElementById("txtIdSueldo").value);
 
-	//Inicializa la variable calculando el 10% de la variable sueldo.
-	let aumento = sueldo * 0.1;
+	//Asigna la variable con el 10%(10/100) de la variable sueldo.
+	aumento = sueldo * 0.1;
 
-	//Inicializa la variable sumando el sueldo con el aumento.
-	let sueldoActualizado = sueldo + aumento;
+	//asigna la variable con la suma del sueldo con el aumento.
+	sueldoActualizado = sueldo + aumento;
 
 	//Cambia el valor de la textbox *txtIdResultado* por el sueldo actualizado
 	document.getElementById("txtIdResultado").value = sueldoActualizado;
