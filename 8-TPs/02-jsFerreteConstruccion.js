1fcdf350652bff539087cad3e7f04da8274379f8
unknown <faustosmiriglia@gmail.com>
2022-07-08 15:59:29 -0300

    Tps - Trabajo 2

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,8 +6,44 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    /*
+    Proposito:
+        -mostrar la cantidad de alambre a comprar  si se ingresara el largo
+         y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
+    */
+    let ancho;
+    let largo;
+    let perimetro;
+    let alambreRequerido;
+    let mensaje;
 
+    largo = parseFloat(document.getElementById("txtIdLargo").value);
+    ancho = parseFloat(document.getElementById("txtIdAncho").value)
+
+    //perimetro = largo * 2 + ancho * 2;
+
+    //perimetro = largo + largo + ancho + ancho;
+
+    perimetro = 2*(largo + ancho);
+
+    alambreRequerido = perimetro * 3;
+
+    mensaje = "La cantidad de alambre requerido es de " + alambreRequerido.toFixed(2) + " Mts";
+    
+    alert(mensaje);
 }
+/*
+    Entrada:
+        -Largo
+        -Ancho
+
+    Proceso:
+        -Calcular perimetro de un rectangulo
+        -Multiplicar el alambre x3
+    
+    Salida:
+        -Alert de el alambre requerido
+*/
 function Circulo () 
 {
 	
