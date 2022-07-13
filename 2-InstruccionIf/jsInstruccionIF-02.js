9398c2263e961be0c239b0b836ab2002e10a81ba
smiriglia <faustosmiriglia@gmail.com>
2022-07-13 14:53:49 -0300

    If - Ejercicio 2

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,16 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	/*
+	Proposito:
+		-Al ingresar una edad debemos informar solo si la persona es mayor de edad
+	*/
+	let edadIngresada;
+	//tomo la edad
+	edadIngresada = document.getElementById("txtIdEdad").value;
+	edadIngresada = parseInt(edadIngresada);
 
+	if (edadIngresada >= 18)
+	{
+		alert("Sos mayor de edad");
+	}
 }//FIN DE LA FUNCIÓN
