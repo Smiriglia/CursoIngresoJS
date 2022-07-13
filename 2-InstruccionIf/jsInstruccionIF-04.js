f5d5c608f13116fb272f098bd68007e4eef3b81d
smiriglia <faustosmiriglia@gmail.com>
2022-07-13 15:15:23 -0300

    If - Ejercicio 4

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,19 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	/*
+	Proposito:
+		-Al ingresar una edad debemos informar si la persona es
+		 adolescente, edad entre 13 y 17 años (inclusive).
+	*/
+	let edadIngresada;
+	//tomo la edad
+	edadIngresada = document.getElementById("txtIdEdad").value;
+	edadIngresada = parseInt(edadIngresada);
+
+	if (edadIngresada >= 13 && edadIngresada <= 17) 
+	{
+		alert("Sos un adolescente");
+	}
 
 
 }//FIN DE LA FUNCIÓN
