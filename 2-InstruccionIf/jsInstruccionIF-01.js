e4c270043d92ab6492401cad25bfc6d5fd3ff06c
smiriglia <faustosmiriglia@gmail.com>
2022-07-13 14:26:23 -0300

    If - Ejercicio 1

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,18 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	/*
+	Proposito:
+		-Al ingresar una edad que sea igual a 15, mostrar el mensaje "niña bonita".
+	*/
+	let edadIngresada;
+	//tomo la edad 
+	edadIngresada = document.getElementById("txtIdEdad").value;
+	edadIngresada = parseInt(edadIngresada);
+
+	if (edadIngresada == 15)
+	{
+		alert("niña bonita");
+	}
+	
 
 }//FIN DE LA FUNCIÓN
