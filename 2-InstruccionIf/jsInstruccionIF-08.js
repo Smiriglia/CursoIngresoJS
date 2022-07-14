71cfabe75631b2552765b85026316d241b818953
smiriglia <faustosmiriglia@gmail.com>
2022-07-14 17:05:25 -0300

    If - Ejercicio 8

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,25 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	/*
+	Proposito:
+		-Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero",
+		 NO HACER NADA, pero si no es asi, y es soltero y no es menor,
+		 mostrar el siguiente mensaje: 'Es soltero y no es menor.'
+	*/
+	let edadIngresada;
+	let estadoCivilIngresado;
 	
+	edadIngresada = document.getElementById("txtIdEdad").value;
+	estadoCivilIngresado = document.getElementById("estadoCivil").value;
 
+	edadIngresada = parseInt(edadIngresada);
+	if(edadIngresada < 18 && estadoCivilIngresado != "Soltero")
+	{
+		
+	}
+	else if(edadIngresada > 17 && estadoCivilIngresado == "Soltero")
+	{
+		alert("Es soltero y no es menor.")
+	}
 
-}//FIN DE LA FUNCIÓN
+}
