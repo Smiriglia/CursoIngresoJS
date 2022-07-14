c92da6702c6c22d0c5374a2cfffb11eaf0dacc8d
smiriglia <faustosmiriglia@gmail.com>
2022-07-14 18:03:19 -0300

    If - Ejercicio 9

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,13 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	/*
+	Proposito:
+		-Al presionar el Botón, mostrar un número Random del 1 al 10 inclusive
+	*/
 
-}//FIN DE LA FUNCIÓN
+	let numeroAleatorio;
+	numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+
+	alert(numeroAleatorio);	
+
+}
