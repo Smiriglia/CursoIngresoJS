a2459823abace2113ec96e568bbd5f9b6e4954a8
smiriglia <faustosmiriglia@gmail.com>
2022-07-14 18:09:15 -0300

    If - Ejercicio 10

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,26 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	/*
+	Proposito:
+		-Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+		 "EXCELENTE" para notas igual a 9 o 10,
+		 "APROBÓ" para notas mayores a 4,
+		 "Vamos, la proxima se puede" para notas menores a 4
+	*/
 
+	let notaGenerada;
+	notaGenerada = Math.floor(Math.random() * 10) + 1;
+
+	if(notaGenerada > 8)
+	{
+		alert("EXCELENTE");
+	}
+	else if(notaGenerada >= 4)
+	{
+		alert("Aprobo");
+	}
+	else
+	{
+		alert("Vamos, la proxima se puede")
+	}
 }//FIN DE LA FUNCIÓN
