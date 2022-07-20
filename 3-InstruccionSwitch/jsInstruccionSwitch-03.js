858b864e15b751d612a84b7c3bfb684a5def5379
smiriglia <faustosmiriglia@gmail.com>
2022-07-20 17:57:04 -0300

    Switch - Ejercicio 3

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,10 +1,23 @@
+/*
+al seleccionar un mes informar.
+si es Febrero: " Este mes no tiene más de 29 días."
+si NO es Febrero: "Este mes tiene 30 o más días"
+*/
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	let mesIngresado = txtIdMes.value;
+	let mensaje;
 
-
-}//FIN DE LA FUNCIÓN
+	switch (mesIngresado)
+	{
+		case "Febrero":
+			mensaje = "Este mes no tiene más de 29 días.";
+			break;
+		
+		default:
+			mensaje = "Este mes tiene 30 o más días";
+	}
+	
+	alert(mensaje);
+}
