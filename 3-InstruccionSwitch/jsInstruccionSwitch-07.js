11fdae62d259297aefe3a86ea8aec9a3a4542b6e
smiriglia <faustosmiriglia@gmail.com>
2022-07-21 14:42:09 -0300

    Switch - Ejercicio 7

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,32 @@
+/*
+Al selecionar un destino , indicar el punto cardinal de nuestro
+pais en donde se encuentra Norte, Sur, Este u Oeste
+*/
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destinoIngresado;
+	let mensaje;
+
+	destinoIngresado =txtIdDestino.value;
+	switch (destinoIngresado)
+	{
+		case "Cataratas":
+			mensaje = "Norte";
+			break;
+		
+		case "Bariloche":
+			mensaje = "Oeste";
+			break;
+		
+		case "Ushuaia":
+			mensaje = "Sur";
+			break;
+
+		case "Mar del plata":
+			mensaje = "Este";
+			break;
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
