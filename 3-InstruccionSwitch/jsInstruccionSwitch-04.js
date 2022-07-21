273546fad72ba2ab40af37b371b7703b33139980
smiriglia <faustosmiriglia@gmail.com>
2022-07-21 14:16:15 -0300

    Switch - Ejercicio 4

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,32 @@
+/*
+al seleccionar un mes informar.
+si tiene 28 días. Febrero
+si tiene 30 días. Abril, Junio, Septiembre y Noviembre.
+si tiene 31 días. Enero, Marzo, Mayo, Julio, Agosto, Octubre y Diciembre.
+*/
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesIngresado  = txtIdMes.value;
+	let mensaje;
 	
-	
+	switch (mesIngresado)
+	{
+		case "Febrero":
+			mensaje = "Este mes tiene 28 días";
+			break;
+		
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			mensaje = "Este mes tiene 30 días";
+			break;
+		
+		default:
+			mensaje = "Este mes tiene 31 días";
+			break;
+	}
 
-
-
-}//FIN DE LA FUNCIÓN
+	alert(mensaje);
+}
