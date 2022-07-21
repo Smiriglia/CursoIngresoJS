3c9600d973946831c4ead3057ccc5f553ae5ce96
smiriglia <faustosmiriglia@gmail.com>
2022-07-21 17:26:46 -0300

    While - Ejercicio 8

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -3,17 +3,34 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
 function mostrar()
 {
-	var contador;
-	var respuesta;
-	var sumaPositivos;
-	var multiplicacionNegativos;
-	contador=0;
-	sumaPositivos=0;
-	multiplicacionNegativos=1;
-	respuesta='si';
+	let contador;
+	let respuesta;
+	let sumaPositivos;
+	let multiplicacionNegativos;
+	let numeroIngresado;
 
+	contador = 0;
+	sumaPositivos = 0;
+	multiplicacionNegativos = 1;
+	respuesta = "si";
 
-	txtIdSuma.value=sumaPositivos;
-	txtIdProducto.value=multiplicacionNegativos;
+	while(respuesta == "si")
+	{
+		numeroIngresado = prompt("Ingrese un numero:");
+		numeroIngresado = parseInt(numeroIngresado);
+		if(numeroIngresado > -1)
+		{
+			sumaPositivos = sumaPositivos + numeroIngresado;
+		}
+		else
+		{
+			multiplicacionNegativos = multiplicacionNegativos * numeroIngresado;
+		}
+		contador++;
+		respuesta = prompt("Quiere seguir ingresando numeros: (si | no)");
+	}
 
-}//FIN DE LA FUNCIÓN
+	txtIdSuma.value = sumaPositivos;
+	txtIdProducto.value = multiplicacionNegativos;
+
+}
