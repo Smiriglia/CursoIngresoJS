19ea6be94fa4c6f67ebd504f4289e022341eb223
smiriglia <faustosmiriglia@gmail.com>
2022-07-22 11:56:11 -0300

    For - Ejercicio 1

--- 6-InstruccionFor/jsIteracionesFor-01.js
+++ 6-InstruccionFor/jsIteracionesFor-01.js
@@ -1,4 +1,10 @@
+/*
+al presionar el botón mostrar 10 repeticiones con números ASCENDENTE, desde el 1 al 10.
+*/
 function mostrar()
 {
-	alert("ok");
+	for(i = 1 ; i < 11; i++ )
+	{
+		console.log(i);
+	}
 }
