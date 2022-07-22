cd2c9e9c0edd7facc3caf8f571601e7520508845
smiriglia <faustosmiriglia@gmail.com>
2022-07-22 12:49:25 -0300

    For - Ejercicio 4

--- 6-InstruccionFor/jsIteracionesFor-04.js
+++ 6-InstruccionFor/jsIteracionesFor-04.js
@@ -1,7 +1,15 @@
+/*
+al presionar el botón repetir hasta que utilizamos 'BREAK'.
+*/
 function mostrar()
 {
-	alert("ok");
-
+	let contador;
+	contador = 0;
+	do
+	{
+		console.log(contador);
+		contador++;
+	}while(contador >0);
 
 
 }//FIN DE LA FUNCIÓN
